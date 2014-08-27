aefb4291acd3164acc6090751b77e1f40bf245a9
Stephen Rosen <sirosen@uchicago.edu>
2014-08-27 12:00:30 -0500

    Replaced version info with PyPi badge

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,23 +1,3 @@
-$(document).ready(
-    $.getJSON("/javascripts/info.json",
-        function ( obj ) {
-            $('.versionstring').each(
-                function (index, elem) {
-                    if (elem.id === "stable") {
-                        elem.innerHTML = "Latest Stable Version " + obj.stable
-                    }
-                    else if (elem.id === "dev") {
-                        elem.innerHTML = "Latest Development Version " + obj.dev
-                    }
-                    else {
-                        elem.innerHTML = ""
-                    }
-                }
-            )
-        }
-    )
-);
-
 $(function() {
     $(".folding-code").accordion({
         collapsible: true,
